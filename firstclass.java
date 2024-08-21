ed05f9bf94111789dcb077162056f4a2a72b56a6
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-08-21 20:14:53 +0530

    hello world

---
+++ firstclass.java
@@ -0,0 +1,8 @@
+class firstclass
+{
+    public static void main(String []args)
+    {
+        System.out.print("bissmillah \n");
+        System.out.print("this is my first program in java language");
+    }
+}
