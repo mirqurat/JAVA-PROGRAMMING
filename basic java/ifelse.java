6746a9328b114667baaf58a7214624847bafef5a
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-10-01 15:45:56 +0530

    if-else

---
+++ basic java/ifelse.java
@@ -0,0 +1,14 @@
+class main{
+    public static void main(String [] args){
+
+      int wether  = 40;
+      if(wether <= 30){
+        System.out.println("it is sunny to day ");
+      }
+      else{
+              System.out.println("it is hot to day ");
+        }
+      
+
+    }
+}
