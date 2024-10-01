7184c676a72b1ffa58c7a5d04f0539d6890071c3
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-10-01 15:46:28 +0530

    looping-statement

---
+++ basic java/loopstatement.java
@@ -0,0 +1,13 @@
+class main{
+    public static void main(String[] args){
+        int num =100;
+        for(int i = 1 ; i<=num;i++){
+            if(i % 2 == 0){
+                System.out.println(i + " is even");
+            }
+            else{
+                System.out.println( i + " is odd");
+            }
+        }
+    }
+}
