70ddb58d41ce06de61391b81d5d40c3d05e70c56
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2025-02-09 11:44:36 +0530

    constructor

---
+++ basic java/constructor.java
@@ -0,0 +1,15 @@
+  class A{
+    int a ;
+    String name;
+    void Show()
+    {
+        System.out.println(a+""+name);
+        
+    }}
+
+class constructor{
+    public static void main(String[]args){
+         A r = new A();
+        r.Show();
+    }
+}
