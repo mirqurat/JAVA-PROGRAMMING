e836ab1f670e05f07cbb45fd5cddf2dc458d1d8f
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2025-02-09 11:43:29 +0530

    array

---
+++ basic java/array.java
@@ -0,0 +1,8 @@
+class Array{
+    public static void main(String args[]){
+        int arr[]= {4,6,7,8,9};
+        for(int i=0;i<=arr.length;i++){
+            System.out.println(i);
+        }
+    }
+}
